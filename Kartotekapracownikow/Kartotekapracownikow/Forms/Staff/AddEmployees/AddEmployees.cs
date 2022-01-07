ab024a91f3e6cd907d4e30431482ee74c9b8ffe8
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2022-01-07 20:29:32 +0100

    Naprawa błędów w formularzu i rozpoczęcie prac nad walidacją formularza Dodaj Pracownika v1.2.1

--- Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/AddEmployees/AddEmployees.cs
+++ Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/AddEmployees/AddEmployees.cs
@@ -35,6 +35,28 @@ namespace Kartotekapracownikow.Forms.AddEmployees
         private AddEmployees()
         {
             InitializeComponent();
+            ValidateData();
+        }
+
+        private void ValidateData()
+        {
+            if (string.IsNullOrWhiteSpace(nazwiskoPracownikaTB.Text))
+            {
+                walidacjaNazwiskoEP.SetError(nazwiskoPracownikaTB, "Pole Nazwisko nie może być puste.");
+            }
+            else
+            {
+                walidacjaNazwiskoEP.Clear();
+            }
+
+            if (string.IsNullOrWhiteSpace(imiePracownikaTB.Text))
+            {
+                walidacjaImieEP.SetError(imiePracownikaTB, "Pole Imię nie może być puste.");
+            }
+            else
+            {
+                walidacjaImieEP.Clear();
+            }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -67,6 +89,40 @@ namespace Kartotekapracownikow.Forms.AddEmployees
         private void anulujBTN_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void dataUrodzinDTP_ValueChanged(object sender, EventArgs e)
+        {
+            dataUrodzinDTP.Format = DateTimePickerFormat.Short;
+        }
+
+        private void imiePracownikaTB_TextChanged(object sender, EventArgs e)
+        {
+            ValidateData();
+        }
+
+        private void nazwiskoPracownikaTB_TextChanged(object sender, EventArgs e)
+        {
+            ValidateData();
+        }
+
+        private void peselTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void peselTB_Validated(object sender, EventArgs e)
+        {
+            string peselHelp = peselTB.Text;
+
+            if (!string.IsNullOrWhiteSpace(peselHelp))
+            {
+                walidacjaPeselEP.SetError(peselTB, "Podaj prawidłową wartość PESEL");
+            }
+            else
+            {
+                walidacjaPeselEP.Clear();
+            }
         }
     }
 }
