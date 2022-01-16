ee1a23f6cc4909637b8590c8e58a0310c3aac358
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2022-01-16 01:48:17 +0100

    Walidacja

--- Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/AddEmployees/AddEmployees.cs
+++ Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/AddEmployees/AddEmployees.cs
@@ -35,28 +35,6 @@ namespace Kartotekapracownikow.Forms.AddEmployees
         private AddEmployees()
         {
             InitializeComponent();
-            ValidateData();
-        }
-
-        private void ValidateData()
-        {
-            if (string.IsNullOrWhiteSpace(nazwiskoPracownikaTB.Text))
-            {
-                walidacjaNazwiskoEP.SetError(nazwiskoPracownikaTB, "Pole Nazwisko nie może być puste.");
-            }
-            else
-            {
-                walidacjaNazwiskoEP.Clear();
-            }
-
-            if (string.IsNullOrWhiteSpace(imiePracownikaTB.Text))
-            {
-                walidacjaImieEP.SetError(imiePracownikaTB, "Pole Imię nie może być puste.");
-            }
-            else
-            {
-                walidacjaImieEP.Clear();
-            }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -91,19 +69,14 @@ namespace Kartotekapracownikow.Forms.AddEmployees
 
         }
 
-        private void dataUrodzinDTP_ValueChanged(object sender, EventArgs e)
-        {
-            dataUrodzinDTP.Format = DateTimePickerFormat.Short;
-        }
-
         private void imiePracownikaTB_TextChanged(object sender, EventArgs e)
         {
-            ValidateData();
+
         }
 
         private void nazwiskoPracownikaTB_TextChanged(object sender, EventArgs e)
         {
-            ValidateData();
+
         }
 
         private void peselTB_KeyPress(object sender, KeyPressEventArgs e)
@@ -111,17 +84,95 @@ namespace Kartotekapracownikow.Forms.AddEmployees
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
+        private void dataUrodzinDTP_ValueChanged(object sender, EventArgs e)
+        {
+            dataUrodzinDTP.Format = DateTimePickerFormat.Short;
+        }
+
         private void peselTB_Validated(object sender, EventArgs e)
         {
             string peselHelp = peselTB.Text;
 
-            if (!string.IsNullOrWhiteSpace(peselHelp))
+            if (string.IsNullOrWhiteSpace(peselHelp))
             {
                 walidacjaPeselEP.SetError(peselTB, "Podaj prawidłową wartość PESEL");
             }
             else
             {
                 walidacjaPeselEP.Clear();
+            }
+        }
+
+        private void nipTB_Validated(object sender, EventArgs e)
+        {
+            /*
+             * Funkcja sprawdza poprawnosc NIP-u
+             * Mnoży każdą liczbę przez wagę
+             * Następnie sprawdza czy ta suma jest równa liczbie kontrolnej
+             * Jeśli tak to NIP jest poprawny, w przeciwnym wypadku NIP jest nieprawidłowy
+             */
+            string nip = nipTB.Text;
+            //System.Diagnostics.Debug.WriteLine(nip);
+
+            if(nip.Length != 10)
+            {
+                walidajcaNipEP.SetError(nipTB, "Numer NIP składa się z 11 cyfr");
+            }
+            else
+            {
+                int[] wagi = new int[] { 6, 5, 7, 2, 3, 4, 5, 6, 7 };
+                int suma = 0;
+
+                string kontrolnaLiczbaSubstring = nip.Substring(9);
+                int kontrolnaLiczba = int.Parse(kontrolnaLiczbaSubstring);
+
+                for (int i = 0; i < wagi.Length; i++)
+                {
+                    suma += (int.Parse(nip.Substring(i, 1)) * wagi[i]);
+                }
+
+                if (suma % 11 != kontrolnaLiczba)
+                {
+                    walidajcaNipEP.SetError(nipTB, "Nieprawidłowy numer NIP");
+                }
+                else
+                {
+                    walidajcaNipEP.Clear();
+                }
+            }
+        }
+
+        private void nazwiskoPracownikaTB_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(nazwiskoPracownikaTB.Text))
+            {
+                walidacjaNazwiskoEP.SetError(nazwiskoPracownikaTB, "Pole Nazwisko nie może być puste.");
+            }
+            else
+            {
+                walidacjaNazwiskoEP.Clear();
+            }
+        }
+
+        private void imiePracownikaTB_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(imiePracownikaTB.Text))
+            {
+                walidacjaImieEP.SetError(imiePracownikaTB, "Pole Imię nie może być puste.");
+            }
+            else
+            {
+                walidacjaImieEP.Clear();
+            }
+        }
+
+        private void dataUrodzinDTP_Validated(object sender, EventArgs e)
+        {
+            DateTime dataUrodzinHelp = Convert.ToDateTime(dataUrodzinDTP.Text);
+
+            if (dataUrodzinHelp >= DateTime.Today)
+            {
+                dataUrodzeniaEP.SetError(dataUrodzinDTP, "Niepoprawna data");
             }
         }
     }
