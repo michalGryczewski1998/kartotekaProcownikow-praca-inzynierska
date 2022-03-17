76a0938a4d1ea66281a683498c28081601e99c12
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2022-03-18 00:27:55 +0100

    Dodawanie pracownika do BD

--- Kartotekapracownikow/Kartotekapracownikow/DatabaseModel/Database.cs
+++ Kartotekapracownikow/Kartotekapracownikow/DatabaseModel/Database.cs
@@ -7,28 +7,85 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Windows;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Logging;
 
 namespace Kartotekapracownikow.DatabaseModel
 {
     public class Database : DbContext
     {
-        
+
         public DbSet<DanePracownikaPodstawowe> DanePracownikaPodstawowe { get; set; }
         public DbSet<DanePracownikaZatrudnienie> DanePracownikaZatrudnienie { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-173Q94A\SQLEXPRESS;Database=KartotekaPracownikow;Trusted_Connection=True;");
 
+            try
+            {
+                optionsBuilder.UseMySQL($"server=localhost;Port=3306;database=kartoteka-pracownicza;user=root;password=SonyW990");               
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Błąd podczas nawiązywania połączenia z bazą danych");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Nieprawidłowy login lub hasło do bazy danych");
+                        break;
+                }
+            }
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<DanePracownikaPodstawowe>()
-                .HasOne(a => a.danePracownikaZatrudnienie)
-                .WithOne(b => b.danePracownika)
-                .HasForeignKey<DanePracownikaZatrudnienie>(b => b.podstawoweRef);
-        }
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<DanePracownikaPodstawowe>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Nazwisko).IsRequired();
+                entity.Property(e => e.Imie).IsRequired();
+                entity.Property(e => e.ZdjeciePracownika).IsRequired();
+                entity.Property(e => e.DataUrodzenia).IsRequired();
+                entity.Property(e => e.NumerTelefonu).IsRequired();
+                entity.Property(e => e.AdresEmail).IsRequired();
+                entity.Property(e => e.MiejsceUrodzenia).IsRequired();
+                entity.Property(e => e.Plec).IsRequired();
+                entity.Property(e => e.ImieMatki).IsRequired();
+                entity.Property(e => e.ImieOjca).IsRequired();
+                entity.Property(e => e.PESEL).IsRequired();
+                entity.Property(e => e.NIP).IsRequired();
+                entity.Property(e => e.Kraj).IsRequired();
+                entity.Property(e => e.Wojewodztwo).IsRequired();
+                entity.Property(e => e.Gmina).IsRequired();
+                entity.Property(e => e.KodZamieszkania).IsRequired();
+                entity.Property(e => e.Miejscowosc).IsRequired();
+                entity.Property(e => e.Ulica).IsRequired();
+                entity.Property(e => e.NumerDomu).IsRequired();
+                entity.Property(e => e.NumerLokalu).IsRequired();
+                entity.Property(e => e.Poczta).IsRequired();
+            });
+
+            modelBuilder.Entity<DanePracownikaZatrudnienie>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.NumerKonta).IsRequired();
+                entity.Property(e => e.Umowa).IsRequired();
+                entity.Property(e => e.Etat).IsRequired();
+                entity.Property(e => e.Bank).IsRequired();
+                entity.Property(e => e.NFZ).IsRequired();
+                entity.Property(e => e.UlgaPodatkowa).IsRequired();
+                entity.Property(e => e.KosztUzyskaniaPrzychodu).IsRequired();
+                entity.Property(e => e.Dzial).IsRequired();
+                entity.Property(e => e.Stanowisko).IsRequired();
+                entity.Property(e => e.StawkaGodzinowa).IsRequired();
+                entity.HasOne(e => e.Podstawowe).WithMany(m => m.Zatrudnienie);
+            });
+        }
     }
 }
