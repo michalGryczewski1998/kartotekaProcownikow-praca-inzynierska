2d58ed4f34ebbe17145e5a2f3a273d54d3a5bd0f
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2022-05-30 20:27:20 +0200

    Zwolnieni pracownicy - informacje o zwolnieniu

--- Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/FormerEmployees/FormerEmployees.Designer.cs
+++ Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/FormerEmployees/FormerEmployees.Designer.cs
@@ -30,32 +30,87 @@ namespace Kartotekapracownikow.Forms.Staff.FormerEmployees
         private void InitializeComponent()
         {
             this.dawniPracownicyGB = new System.Windows.Forms.GroupBox();
+            this.OdswiezBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.zaznaczonyPracownikLabel = new System.Windows.Forms.Label();
+            this.DaneZwolnienieDGW = new System.Windows.Forms.DataGridView();
+            this.dawniPracownicyGB.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DaneZwolnienieDGW)).BeginInit();
             this.SuspendLayout();
             // 
             // dawniPracownicyGB
             // 
+            this.dawniPracownicyGB.Controls.Add(this.DaneZwolnienieDGW);
             this.dawniPracownicyGB.Location = new System.Drawing.Point(12, 12);
             this.dawniPracownicyGB.Name = "dawniPracownicyGB";
-            this.dawniPracownicyGB.Size = new System.Drawing.Size(776, 283);
+            this.dawniPracownicyGB.Size = new System.Drawing.Size(936, 367);
             this.dawniPracownicyGB.TabIndex = 0;
             this.dawniPracownicyGB.TabStop = false;
             this.dawniPracownicyGB.Text = "Byli Pracownicy";
+            // 
+            // OdswiezBTN
+            // 
+            this.OdswiezBTN.Location = new System.Drawing.Point(870, 382);
+            this.OdswiezBTN.Name = "OdswiezBTN";
+            this.OdswiezBTN.Size = new System.Drawing.Size(75, 23);
+            this.OdswiezBTN.TabIndex = 1;
+            this.OdswiezBTN.Text = "Odśwież";
+            this.OdswiezBTN.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pracownik: ";
+            // 
+            // zaznaczonyPracownikLabel
+            // 
+            this.zaznaczonyPracownikLabel.AutoSize = true;
+            this.zaznaczonyPracownikLabel.Location = new System.Drawing.Point(80, 390);
+            this.zaznaczonyPracownikLabel.Name = "zaznaczonyPracownikLabel";
+            this.zaznaczonyPracownikLabel.Size = new System.Drawing.Size(0, 15);
+            this.zaznaczonyPracownikLabel.TabIndex = 3;
+            // 
+            // DaneZwolnienieDGW
+            // 
+            this.DaneZwolnienieDGW.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.DaneZwolnienieDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DaneZwolnienieDGW.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DaneZwolnienieDGW.Location = new System.Drawing.Point(3, 19);
+            this.DaneZwolnienieDGW.Name = "DaneZwolnienieDGW";
+            this.DaneZwolnienieDGW.RowTemplate.Height = 25;
+            this.DaneZwolnienieDGW.Size = new System.Drawing.Size(930, 345);
+            this.DaneZwolnienieDGW.TabIndex = 0;
+            this.DaneZwolnienieDGW.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DaneZwolnienieDGW_CellContentClick);
             // 
             // FormerEmployees
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 450);
+            this.Controls.Add(this.zaznaczonyPracownikLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OdswiezBTN);
             this.Controls.Add(this.dawniPracownicyGB);
             this.Name = "FormerEmployees";
             this.Text = "FormerEmployees";
             this.Load += new System.EventHandler(this.FormerEmployees_Load);
+            this.dawniPracownicyGB.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DaneZwolnienieDGW)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox dawniPracownicyGB;
+        private System.Windows.Forms.Button OdswiezBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label zaznaczonyPracownikLabel;
+        private System.Windows.Forms.DataGridView DaneZwolnienieDGW;
     }
 }
--- Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/FormerEmployees/FormerEmployees.cs
+++ Kartotekapracownikow/Kartotekapracownikow/Forms/Staff/FormerEmployees/FormerEmployees.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kartotekapracownikow.DatabaseModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,9 @@ namespace Kartotekapracownikow.Forms.Staff.FormerEmployees
     {
 
         private static FormerEmployees _instance = null;
+
+        int employeesID;
+        string imieZwolnionego;
 
         public static FormerEmployees Instance
         {
@@ -42,10 +46,59 @@ namespace Kartotekapracownikow.Forms.Staff.FormerEmployees
         private FormerEmployees()
         {
             InitializeComponent();
+            DaneZwolnieniPracownicyWyswietl();      
         }
 
         private void FormerEmployees_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void DaneZwolnieniPracownicyWyswietl()
+        {
+            using(var db = new Database())
+            {
+                try
+                {
+                    var query = (from collection in db.ZwolnieniPracownicy
+                                 select new
+                                 {
+                                     collection.ID,
+                                     collection.Imie,
+                                     collection.Nazwisko,
+                                     collection.Kraj,
+                                     collection.NumerTelefonu,
+                                     collection.Dzial,
+                                     collection.Stanowisko,
+                                     collection.DataRozpoczeciaPracy,
+                                     collection.DataZakonczeniaPracy
+                                 }).ToList();
+
+                    DaneZwolnienieDGW.DataSource = query;
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Błąd podczas pobierania danych o zwolnionych pracownikach");
+                }
+            }
+        }
+
+        private void DaneZwolnienieDGW_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = DaneZwolnienieDGW.Rows[e.RowIndex];
+                    employeesID = (int)row.Cells["ID"].Value;
+                    imieZwolnionego = row.Cells["Imie"].Value.ToString();
+                    zaznaczonyPracownikLabel.Text = row.Cells["Imie"].Value.ToString();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Błąd podczas pobierania danych o zwolnionych pracownikach");
+            }
 
         }
     }
